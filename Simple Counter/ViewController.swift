1d3684f640287616ffca2153aae3cfd13a6fa63b
kostyaalehin <koss595959@gmail.com>
2022-05-17 22:27:32 +0500

    Initial Commit

---
+++ Simple Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Counter
+//
+//  Created by Константин Алехин on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
